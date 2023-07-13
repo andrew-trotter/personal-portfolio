fe8a404baeea9f41ba4c2ba9bf11212835f37aea
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-13 18:34:03 +1200

    Fixed for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
 module.exports = {
   content: ['./client/**/*.{html,jsx,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
   },
   plugins: [],
 }
