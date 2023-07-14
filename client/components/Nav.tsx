1ff09f0e8fba09eb8727f64a5420386d4b147d35
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-14 16:00:11 +1200

    feat: scroll to same page navigation

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -1,9 +1,31 @@
+import { useEffect, useState } from 'react';
+import {Link } from 'react-router-dom'
+
 function Nav() {
+  const [path, setPath] = useState('')
+
+  function handleNav(input: string){
+    setPath(input)
+  }
+
+  useEffect(() => {
+    const href = window.location.href.substring(
+      window.location.href.lastIndexOf('#') + 1
+    );
+    
+    const element = document.getElementById(href);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [path]);
+
   return (
       <nav className="my-14 ">
         <ul className="flex flex-col gap-4">
-          <li>-- About</li>
-          <li>-- Projects</li>
+          <Link to={"#about"} onClick={() => handleNav("#about")}>
+            <li>-- About</li>
+          </Link>
+          <li><Link to={"#projects"} onClick={() => handleNav("#projects")}>-- Projects</Link></li>
         </ul>
       </nav>
   )
