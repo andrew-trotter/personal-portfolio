fa56eedb71719f9bbd3be4a36b7719d15d9e28ce
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-14 14:56:51 +1200

    style: changed to li

--- client/components/ProjectItem.tsx
+++ client/components/ProjectItem.tsx
@@ -9,18 +9,20 @@ function ProjectItem(props: Props) {
   const project = props.project
 
   return (
-    <div className="">
-      {/* <img src={project.image} alt={project.name} /> */}
-      <div>
-        <h4 className='text-white'>{project.name}</h4>
-        <h5>{project.desc}</h5>
-        <div className="features">
-          {project.techs.map((tech) => (
-            <FeatureItem key={tech.name} name={tech.name} />
-          ))}
+    <li className="flex">
+      <div className=''>
+        <img src={project.image} alt={project.name} />
+        <div>
+          <h4 className="text-white">{project.name}</h4>
+          <h5>{project.desc}</h5>
+          <div className="features">
+            {project.techs.map((tech) => (
+              <FeatureItem key={tech.name} name={tech.name} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </li>
   )
 }
 
