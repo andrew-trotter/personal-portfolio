c84147dfa81962a46e937c31b01077d21d6db875
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-14 15:20:33 +1200

    style: basic styling

--- client/components/ProjectItem.tsx
+++ client/components/ProjectItem.tsx
@@ -9,17 +9,21 @@ function ProjectItem(props: Props) {
   const project = props.project
 
   return (
-    <li className="flex">
-      <div className=''>
-        <img src={project.image} alt={project.name} />
+    // TODO: added frosted glass background on hover
+    <li className=" my-12 ">
+      <div className="flex gap-4">
+        <img
+          className="object-scale-down ml-2 mt-2 self-start"
+          src={project.image}
+          alt={project.name}
+        />
         <div>
-          <h4 className="text-white">{project.name}</h4>
+          <h4 className="text-lg font-bold text-fuchsia-500">{project.name}</h4>
           <h5>{project.desc}</h5>
-          <div className="features">
-            {project.techs.map((tech) => (
-              <FeatureItem key={tech.name} name={tech.name} />
-            ))}
-          </div>
+
+          {project.techs.map((tech) => (
+            <FeatureItem key={tech.name} name={tech.name} />
+          ))}
         </div>
       </div>
     </li>
