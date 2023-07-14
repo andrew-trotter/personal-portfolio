84dfec9c6780506221e6b98fc846f6121b77e14e
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-14 15:19:32 +1200

    style: features styled

--- client/components/FeatureItem.tsx
+++ client/components/FeatureItem.tsx
@@ -4,7 +4,9 @@ interface Props {
 
 function FeatureItem({name}: Props) {
   return(
-    <div className="feature-item">{name}</div>
+    <div className="inline-flex border-0 bg-fuchsia-500 bg-opacity-10 mx-1 my-1 py-0.5 px-2 rounded-full ">
+      <div className="text-fuchsia-500">{name}</div>
+      </div>
   )
 }
 
