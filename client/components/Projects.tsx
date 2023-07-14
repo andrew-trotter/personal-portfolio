fb93bad2c9e7286c9f64ff31400e614eea229f39
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-14 16:00:54 +1200

    feat: added ids for nav

--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -4,7 +4,7 @@ import ProjectItem from "./ProjectItem"
 function Projects() {
   const data = projects
   return (
-    <ul className="my-14">
+    <ul id="projects" className="my-14">
       {data.map(project => <ProjectItem key={project.id} project={project}/>)}
     </ul>
   )
