fa04bb6b41c1395cb8ad92696df6e587759982f9
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-16 12:12:56 +1200

    style: added hover effects

--- client/components/ProjectItem.tsx
+++ client/components/ProjectItem.tsx
@@ -11,7 +11,7 @@ function ProjectItem(props: Props) {
   return (
     // TODO: added frosted glass background on hover
     <li className=" my-12 ">
-      <div className="flex gap-4">
+      <div className="flex gap-4 p-4 rounded-lg hover:bg-slate-500 hover:bg-opacity-10 hover:shadow-2xl duration-300 ease-in-out">
         <img
           className="object-scale-down ml-2 mt-2 self-start"
           src={project.image}
@@ -19,7 +19,7 @@ function ProjectItem(props: Props) {
         />
         <div>
           <h4 className="text-lg font-bold text-fuchsia-500">{project.name}</h4>
-          <h5>{project.desc}</h5>
+          <h5 className="text-gray-400">{project.desc}</h5>
 
           {project.techs.map((tech) => (
             <FeatureItem key={tech.name} name={tech.name} />
--- client/components/Projects.tsx
+++ client/components/Projects.tsx
@@ -1,10 +1,10 @@
-import projects from "../data/data"
+import {projects} from "../data/data"
 import ProjectItem from "./ProjectItem"
 
 function Projects() {
   const data = projects
   return (
-    <ul id="projects" className="my-14">
+    <ul id="projects" className="my-24">
       {data.map(project => <ProjectItem key={project.id} project={project}/>)}
     </ul>
   )
