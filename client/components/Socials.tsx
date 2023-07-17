2d644f0686ab6fbbda718f1f4288f98fd20a9190
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-17 20:19:50 +1200

    style: added hover animations

--- client/components/Socials.tsx
+++ client/components/Socials.tsx
@@ -6,8 +6,8 @@ config.autoAddCss = false
 function Socials() {
   return (
     <div className="flex gap-4 text-gray-400 justify-self-end">
-      <a href="https://github.com/andrew-trotter" target="_blank" rel="noreferrer" ><span className="fa-brands fa-github fa-xl"></span></a>
-      <a href="https://www.linkedin.com/in/andrew-trotter-1918a0102/" target="_blank" rel="noreferrer" ><span className="fa-brands fa-linkedin fa-xl"></span></a>
+      <a href="https://github.com/andrew-trotter" target="_blank" rel="noreferrer" ><span className="fa-brands fa-github fa-xl hover:text-fuchsia-500 duration-300 ease-in-out"></span></a>
+      <a href="https://www.linkedin.com/in/andrew-trotter-1918a0102/" target="_blank" rel="noreferrer" ><span className="fa-brands fa-linkedin fa-xl hover:text-fuchsia-500 duration-300 ease-in-out"></span></a>
     </div>
   )
 }
