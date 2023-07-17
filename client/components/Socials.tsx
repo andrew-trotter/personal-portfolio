91719fdf4817bb4fe25d8c520ee67debe7a45b2a
andrew-trotter <andrewjtrotter@gmail.com>
2023-07-17 17:40:35 +1200

    style: added icons with links

--- client/components/Socials.tsx
+++ client/components/Socials.tsx
@@ -1,8 +1,13 @@
+import '@fortawesome/fontawesome-free/css/all.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import '@fortawesome/fontawesome-svg-core/styles.css'
+config.autoAddCss = false
+
 function Socials() {
   return (
-    <div className="justify-self-end">
-      <div>Github</div>
-      <div>Linkedin</div>
+    <div className="flex gap-4 text-gray-400 justify-self-end">
+      <a href="https://github.com/andrew-trotter" target="_blank" rel="noreferrer" ><span className="fa-brands fa-github fa-xl"></span></a>
+      <a href="https://www.linkedin.com/in/andrew-trotter-1918a0102/" target="_blank" rel="noreferrer" ><span className="fa-brands fa-linkedin fa-xl"></span></a>
     </div>
   )
 }
